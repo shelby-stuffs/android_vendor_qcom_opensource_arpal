f6729bf56d7a49d7f82d97f0b0b9e19652ab179d
Bruno Martins <bgcngm@gmail.com>
2023-05-11 15:32:05 +0100

    pal_ipc_server: Skip on OSS builds
    Change-Id: If244a30b36333dabf150d726de6430af521079ba

--- ipc/HwBinders/pal_ipc_server/Android.mk
+++ ipc/HwBinders/pal_ipc_server/Android.mk
@@ -1,4 +1,7 @@
 LOCAL_PATH := $(call my-dir)
+
+ifneq ($(QCPATH),)
+
 include $(CLEAR_VARS)
 
 LOCAL_MODULE := vendor.qti.hardware.pal@1.0-impl
@@ -33,3 +36,4 @@ LOCAL_SHARED_LIBRARIES := \
 
 include $(BUILD_SHARED_LIBRARY)
 
+endif
